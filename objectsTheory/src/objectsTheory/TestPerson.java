f36230f858102b6d176a8a3cbe9700281eeff265
admin CIFO <admin CIFO@DESKTOP-NG7OTN7>
2021-05-26 12:02:48 +0200

    new comments

--- objectsTheory/src/objectsTheory/TestPerson.java
+++ objectsTheory/src/objectsTheory/TestPerson.java
@@ -1,20 +1,20 @@
 package objectsTheory;
 
 public class TestPerson {
-	
-	//call method type main that does not return anything
+
+	// call method type main that does not return anything
 	public static void main(String[] args) {
 
 		// I declare the isa variable of type Person and assign it to the new person
 		// object that sends (isabel)
 		Person isa = new Person("Isabel");
-		//1. Is Calling a method operation changeReference();
-		//2. Sending an Person object call "isa" for operation or evaluation.
-		//3. The method is written as changeReference(isa);
+		// Is Calling a method operation changeReference();
+		// Sending an Person object call "isa" for operation or evaluation.
+		// The method is written as changeReference(isa);
 		changeReference(isa);
-		//Porque? usamos método modifyReference() para modificar objeto;
-		//Con qué? con objeto isa de tipo User;
-		//Cómo?  modifyReference(isa)
+		// we use modifyReference () method to modify object;
+		// with isa object of type User;
+		// to modifyReference (isa)
 		modifyReference(isa);
 	}
 
@@ -22,12 +22,15 @@ public class TestPerson {
 	// and we declare object of type Person named objectPersonToChange (an argument
 	// for the method)
 	public static void changeReference(Person objectPersonToChange) {
+		
 		// I declare the borja variable which is of type Person and assign the value new
 		// Peron ("Borja")
 		Person borja = new Person("Borja");
-		//In the modifyReference method, which receives a Person Object as a parameter, we call that parameter, 
-		//and lift the setName () method, previously defined in the Person class, and in the setName () method parameter 
-		//we introduce the String to which we want to change
+		// In the modifyReference method, which receives a Person Object as a parameter,
+		// we call that parameter,
+		// and lift the setName () method, previously defined in the Person class, and
+		// in the setName () method parameter
+		// we introduce the String to which we want to change
 		objectPersonToChange = borja;
 	}
 
@@ -35,6 +38,9 @@ public class TestPerson {
 	// objectPersonToModify” as parameter, in order to modify the sent objects.
 	public static void modifyReference(Person objectPersonToModify) {
 
+		//we get object objectPersonToModify and set its Name attribute to "Sergii" trough method setName
+		//objectPersonToModify is a reference-variable which points to object isa (which points to object new Person())
+		//that executes its method setName a change its value to  "Sergii"
 		objectPersonToModify.setName("Sergii");
 	}
 
